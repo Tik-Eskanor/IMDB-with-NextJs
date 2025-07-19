e23ec0af32a7a60b30adfaa859c6f03f746ab1ce
Ade <tik.eskanor@gmail.com>
2025-07-19 12:19:43 +0100

    Push commit

--- lib/mongodb/mongoose.tsx
+++ lib/mongodb/mongoose.tsx
@@ -2,7 +2,6 @@ import mongoose, { Mongoose } from "mongoose";
 
 
 export default function connect() {
-    mongoose.set('debug', true);
     mongoose.connect(process.env.MONGODB_URI || "")
         .then(() => console.log('Connected to:', mongoose.connection.name))
         .catch(err => console.error('Connection error:', err));
